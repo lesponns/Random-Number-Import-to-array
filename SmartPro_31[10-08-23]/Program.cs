f34e2f4052822254a2088d1c84e20cfb64c6908a
lesponns <adamadakadam1903@gmail.com>
2023-08-10 21:15:40 +0300

    Add project files.

---
+++ SmartPro_31[10-08-23]/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPro_31_10_08_23_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int num;
+
+            int[] numbers = new int[20];
+
+            for (int i = 0; i < numbers.Length; i++) 
+            {
+                num = rnd.Next(0, 100);
+                numbers[i] = num;
+                if (numbers[i] < 50)
+                {
+                    Console.WriteLine("{0}", numbers[i]);
+                }
+                
+                
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
